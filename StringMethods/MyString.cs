b4c1ed720b081aef2dec90b006004438d2caee22
CemileIsmayilZada <5cctf7f@code.edu.az>
2023-04-27 19:34:09 -0500

    added codes

---
+++ StringMethods/MyString.cs
@@ -0,0 +1,112 @@
+﻿using System.Text;
+
+namespace StringMethods
+{
+	public class MyString
+	{
+
+		#region Split
+		public string[] SplitArray(string str, char character = ' ')
+		{
+			string[] myarr = new string[str.Length + 1];
+
+			int arrayCount = 0;
+			StringBuilder word = new StringBuilder();
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (str[i] != character)
+				{
+					word.Append(str[i]);
+					if (i == str.Length - 1)
+					{
+						myarr[arrayCount] = word.ToString();
+						arrayCount++;
+					}
+					continue;
+				}
+
+				myarr[arrayCount] = word.ToString();
+				word = new StringBuilder();
+				arrayCount++;
+			}
+			Array.Resize(ref myarr, arrayCount);
+
+			return myarr;
+
+		}
+		#endregion
+
+		#region Substring
+
+		#region Substring(int startIndex)
+		public string MySubstring(string word, int index)
+		{
+			if (index < 0)
+			{
+				throw new Exception("StartIndex cannot be less than zero");
+			}
+			else if (index > word.Length)
+			{
+				throw new Exception("StartIndex cannot be larger than length of string");
+			}
+			StringBuilder result = new StringBuilder();
+			for (int i = index; i < word.Length; i++)
+			{
+				result.Append(word[i]);
+			}
+			return result.ToString();
+		}
+		#endregion
+
+		#region Substring(int index, int length)
+
+		public string MySubstring(string word, int index, int length)
+		{
+			if (index < 0)
+			{
+				throw new Exception("StartIndex cannot be less than zero");
+			}
+			else if (index > word.Length)
+			{
+				throw new Exception("StartIndex cannot be larger than length of string");
+			}
+			if (length < 0)
+			{
+				throw new Exception("Length cannot be less than zero");
+			}
+			else if (index + length > word.Length)
+			{
+				throw new Exception("Index and length must refer to a location within the string. ");
+			}
+
+			StringBuilder result = new StringBuilder();
+			for (int i = index; i < index + length; i++)
+			{
+				result.Append(word[i]);
+			}
+			return result.ToString();
+		}
+
+		#endregion
+		#endregion
+
+		#region Replace
+
+		public string MyReplace(string word, char oldChar, char newChar)
+		{
+			StringBuilder result = new StringBuilder();
+			for (int i = 0; i < word.Length; i++)
+			{
+				if (word[i] == oldChar)
+				{
+					result.Append(newChar);
+					continue;
+				}
+				result.Append(word[i]);
+			}
+			return result.ToString();
+		}
+		#endregion
+
+	}
+}
---
+++ StringMethods/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Hello World");
+
+
+#region Split(char c)
+//string str = "aaaa";
+//string[] arr = str.Split('a');//
+//foreach (var item in arr)
+//{
+//	Console.WriteLine(item + 1);
+//}
+
+
+//string str = "Alma.Armud.Heyva";
+//MyString myString= new MyString();
+//string[] answerArray = myString.SplitArray(str, 'A');
+//foreach (var item in answerArray)
+//{
+//	Console.WriteLine(item);
+//}
+#endregion
+
+
+#region Substring
+
+#region Substring(int startIndex)
+//string str = "Alma";
+//Console.WriteLine(str.Substring(5)); 
+
+//MyString myString = new MyString();
+//Console.WriteLine(myString.MySubstring(str, 4));
+#endregion
+
+#region Substring(int index, int length)
+
+//string str = "Alma";
+//Console.WriteLine(str.Substring(1,4));
+
+//string str = "Alma";
+//MyString myString=new MyString();
+//Console.WriteLine(myString.MySubstring(str,1,3));
+#endregion
+#endregion
+
+
+#region Replace
+
+//string str = "nana";
+//Console.WriteLine(str.Replace('s', 'b'));
+
+//string str = "nana";
+//MyString myString = new MyString();
+//Console.WriteLine(myString.MyReplace(str,'n', '.'));
+#endregion
